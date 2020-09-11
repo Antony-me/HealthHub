c1b6ba24a13107f582a652aa71687fa8f3a4b290
Antony-me <68530949+Antony-me@users.noreply.github.com>
2020-09-11 10:55:51 +0300

    Development (#10)
    * added files
    
    * added about.html
    
    * added about.html (#2)
    
    Co-authored-by: Antony-me <68530949+Antony-me@users.noreply.github.com>
    
    * added a paragraph
    
    * final (#4)
    
    * Health hubabout (#6)
    
    * changed directory of about.html
    
    * linked content to about.css
    
    * added a description
    
    * added images and styled my work
    
    * final commit
    
    * rectified correction in styles
    
    * added a navbar
    
    * final landing page (#5)
    
    * final landing page
    
    * changed css to styles
    
    * Healthdiag (#8)
    
    * Added java script and bootstrap files
    
    * Added symptoms checkboxes
    
    * Added filtering js for symptoms
    
    * Fixed a bug in the js
    
    * Styles the symptoms checker card
    
    * Added styles to my diagnosis
    
    Co-authored-by: Moringa School Access <shebaz3000@gmail.com>
    Co-authored-by: Jane Adhiambo <68597204+JaneAdhiambo3000@users.noreply.github.com>
    Co-authored-by: Kelvin Washiko <68896930+Washikokevv27@users.noreply.github.com>
    Co-authored-by: charlesmugambi <69636375+charlesmugambi@users.noreply.github.com>

---
+++ js/diagnos.js
@@ -0,0 +1,64 @@
+var metrixSy= 0;
+var metrixCly = 0;
+var metrixGono = 0;
+
+function is_checked(){
+  var syphilisA =document.getElementById('rash').checked;
+  var syphilisB =document.getElementById('fever').checked;
+  var syphilisC =document.getElementById('Weight-loss').checked;
+  var syphilisD =document.getElementById('hair-loss').checked;
+  var syphilisE =document.getElementById('joint-pain').checked;
+
+  var chlymydiaA =document.getElementById('pain-sex')
+  var chlymydiaB =document.getElementById('green-yellow')
+  var chlymydiaC =document.getElementById('lower-pain')
+  var chlymydiaD =document.getElementById('bleeding')
+  var chlymydiaE=document.getElementById('testicles')
+
+
+  var gonorrheaA = document.getElementById('yello-discharge');
+  var gonorrheaB = document.getElementById('pain-urinating');
+  var gonorrheaC = document.getElementById('urinating-unusal');
+  var gonorrheaD = document.getElementById('itching');
+  var gonorrheaE = document.getElementById('sore-throat');
+
+  var newSyphilis = [syphilisA, syphilisB, syphilisC, syphilisD, syphilisE];
+  var newChlymydia = [chlymydiaA, chlymydiaB, chlymydiaC, chlymydiaD, chlymydiaE];
+  var newGono =[gonorrheaA, gonorrheaB,gonorrheaC, gonorrheaD, gonorrheaE];
+
+  var newDisease =[newSyphilis, newChlymydia, newGono]
+
+  newSyphilis.forEach(function(symp){
+    for(i=0;i< 5; i ++); 
+    if( symp == true){
+      metrixSy +=1;
+    }
+    else{
+      return false;
+    }
+  })  
+  alert('You have ' + metrixSy + ' symptom(s) for Syphilis.. ' );
+
+  newChlymydia.forEach(function(smp){
+    for(i=0;i< 5; i ++); 
+    if( smp == true){
+      metrixCly +=1;
+    }
+    else{
+      return false;
+    }
+  })  
+  alert( '  You have ' + metrixCly + ' symptom(s) for Chlymydia... ');
+
+  newGono.forEach(function(gono){
+    for(i=0;i< 5; i ++); 
+    if( gono == true){
+      metrixGono +=1;
+    }
+    else{
+      return false;
+    }
+  })  
+  alert( '  You have ' + metrixCly + ' symptom(s) for Gonorrhea ... ');
+
+}
